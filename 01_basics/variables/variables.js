82d71d42d1736e26cf2bc59b5b4381ca436ca0e0
Syed Zohaib Shah <154647656+zohaibshah7@users.noreply.github.com>
2024-12-23 10:38:51 +0000

    practicing variables with investigation

---
+++ 01_basics/variables/variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345;
+let myAccountEmail = "zohaibshah7@gmail.com"
+var myAccountPassword = "123456"
+myCity = "karachi"
+let myState; // This will show undefined
+
+/*
+    myCity = "lahore";
+    console.log(myCity); // This is not good practice
+*/
+
+// accountId = 2345 // This is not allowed
+// console.log(accountId);
+
+console.table([accountId, myAccountEmail, myAccountPassword, myCity, myState])
