f31e1f601df4a72b2e350d3eb279734e5840d310
Syed Zohaib Shah <154647656+zohaibshah7@users.noreply.github.com>
2024-12-27 13:21:51 +0000

    Handling objects in function

--- 01_basics/functions.js
+++ 01_basics/functions.js
@@ -23,10 +23,40 @@ function loginUserMessage(username) {
     //     return
     // }
     if (!username) {
-        console.log("Please enter username first");
+        // console.log("Please enter username first");
         return
     }
-    return `${username} user is logged in`
+    // return `${username} user is logged in`
 }
 console.log(loginUserMessage());
 // console.log(loginUserMessage("Zohaib"));
+
+function calculateCartPrice(val1, val2, ...val) {
+    return val
+}
+// console.log(calculateCartPrice(100));
+// console.log(calculateCartPrice(100, 200, 3000, 400, 500, 600));
+
+const user = {
+    username: 'Zohaib',
+    price: 999
+}
+
+function handleObject(anyObject) {
+    // console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: 'Soldier boy',
+    price: 499
+})
+
+const myNewArray = [100, 200, 300, 4000, 5000]
+
+function returnSecondValue(getArray) {
+    return getArray
+    // console.log(getArray);
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([100, 200, 300, 400, 500]));
