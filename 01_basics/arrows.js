8acf567839ca3fa38979cecf5a99d3ca2602f4fd
Syed Zohaib Shah <154647656+zohaibshah7@users.noreply.github.com>
2024-12-30 11:05:40 +0000

    Arrow function practicing

--- 01_basics/arrows.js
+++ 01_basics/arrows.js
@@ -7,6 +7,31 @@ const user = {
   },
 };
 
-user.message();
-user.username = "Soldier boy";
-user.message();
+// user.message();
+// user.username = "Soldier boy";
+// user.message();
+
+////////// Arrow function
+
+const aboutMe = () => {
+  let username = "Zohaib";
+  console.log(this);
+};
+
+// aboutMe()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({ username: "Zohaib" });
+
+console.log(addTwo(3, 4));
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
