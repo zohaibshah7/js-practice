fbc8dbf0668060cdec144b61c05de63f71209bf4
Syed Zohaib Shah <154647656+zohaibshah7@users.noreply.github.com>
2025-02-06 15:44:29 +0000

    try catch error message

--- 01_basics/numbers_and_maths.js
+++ 01_basics/numbers_and_maths.js
@@ -1,34 +1,74 @@
-const score = 400;
-console.log(score);
+// const score = 400;
+// console.log(score);
 
-const balance = new Number(100);
-console.log(balance);
+// const balance = new Number(100);
+// console.log(balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(1));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
 
-const otherNumber = 123.8966;
+// const otherNumber = 123.8966;
 
-console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(4));
 
-const hundreds = 1000000;
-console.log(hundreds.toLocaleString('en-IN'));
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
 
-// +++++++++++++ Maths +++++++++++++++++++++++++++++
+// // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4.6));
-console.log(Math.ceil(4.2));
-console.log(Math.floor(4.9));
-console.log(Math.min(4, 3, 6, 8));
-console.log(Math.max(4, 3, 6, 8));
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
 
-console.log(Math.random());
-console.log(Math.random() * 10 + 1);
-console.log(Math.floor(Math.random() * 10) + 1);
+// console.log(Math.random());
+// console.log(Math.random() * 10 + 1);
+// console.log(Math.floor(Math.random() * 10) + 1);
 
-const min = 33;
-const max = 40;
+// const min = 33;
+// const max = 40;
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+// let num1 = 10
+// let num2 = 20
+
+// let sum = num1 + num2
+
+// console.log("",sum);
+
+let num = 10;
+
+// if (num % 2 === 0) {
+//     console.log(num, "is even");
+// } else {
+//     console.log(num, "is odd");
+// }
+
+let num1 = 20
+// let num2 = 30
+
+// if (num1 > num2) {
+//     console.log(num1, "is greater");
+// } else {
+//     console.log(num2, "is greater");
+// }
+
+// for (let i = 1; i <= 10; i++) {
+// //     console.log(num + " x " + i + " = " + (num * i));
+// // }
+
+// // for (let i = 1; i <= 10; i++) {
+// //     console.log(num1 + " x " + i + " = " + (num1 * i));
+// // }
+
+try {
+    let result = 10 / 0; // Yeh error nahi dega, par maan lo koi aur error hota
+    console.log(result);
+    let a = b + 10; // Yeh error dega kyunki b defined nahi hai
+  } catch (error) {
+    console.log("Error aagaya bhai!", error.message); // Yeh error ko pakad lega
+  }
